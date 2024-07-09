694b876274d7329acaa24c9daee6df7d9dda09e6
Malik Sohaib Iqbal <163728585+MalikSohaibIqbal@users.noreply.github.com>
2024-07-10 01:11:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let difference;
 let tInterval;
 let running = false;
 let lapNumber = 1;
+
 const startStopBtn = document.getElementById('startStopBtn');
 const resetBtn = document.getElementById('resetBtn');
 const lapBtn = document.getElementById('lapBtn');
@@ -11,6 +12,34 @@ const minutesDisplay = document.getElementById('minutes');
 const secondsDisplay = document.getElementById('seconds');
 const millisecondsDisplay = document.getElementById('milliseconds');
 const lapsList = document.getElementById('laps');
+
 function startStop() {
     if (!running) {
         startTime = new Date().getTime() - (difference || 0);
+        tInterval = setInterval(getShowTime, 10);
+        startStopBtn.innerHTML = "Pause";
+        running = true;
+    } else {
+        clearInterval(tInterval);
+        startStopBtn.innerHTML = "Start";
+        running = false;
+    }
+}
+
+function reset() {
+    clearInterval(tInterval);
+    startTime = 0;
+    updatedTime = 0;
+    difference = 0;
+    running = false;
+    startStopBtn.innerHTML = "Start";
+    minutesDisplay.innerHTML = "00";
+    secondsDisplay.innerHTML = "00";
+    millisecondsDisplay.innerHTML = "00";
+    lapsList.innerHTML = "";
+    lapNumber = 1;
+}
+
+function lap() {
+    if (running) {
+        const lapTime = `${minutesDisplay.innerHTML}:${secondsDisplay.innerHTML}:${millisecondsDisplay.innerHTML}`;
