35760f00534893ec932404fca19e554ff2970b15
Malik Sohaib Iqbal <163728585+MalikSohaibIqbal@users.noreply.github.com>
2024-07-10 01:10:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let difference;
 let tInterval;
 let running = false;
 let lapNumber = 1;
+const startStopBtn = document.getElementById('startStopBtn');
+const resetBtn = document.getElementById('resetBtn');
+const lapBtn = document.getElementById('lapBtn');
+const minutesDisplay = document.getElementById('minutes');
+const secondsDisplay = document.getElementById('seconds');
+const millisecondsDisplay = document.getElementById('milliseconds');
+const lapsList = document.getElementById('laps');
+function startStop() {
+    if (!running) {
+        startTime = new Date().getTime() - (difference || 0);
